e714fbdea3ff800691ab0f90e01fb1e2fd192573
Mario Roberti <mario.roberti@gmail.com>
2012-10-27 10:37:25 -0700

    Altered the main.lua file to adjust for faster message creation, removed the 'require ciderdebugger.lua' chunk too.

--- main.lua
+++ main.lua
@@ -1,4 +1,3 @@
-
 -- Example usage:
 local MessageQueue = require( "MessageQueue")
 local myMessageQueue = MessageQueue.new()
@@ -9,12 +8,18 @@ screenH = display.contentHeight
 centerX  = display.contentWidth/2
 centerY = display.contentHeight/2
 
+-- Called from the 'CreateMessage method on successful completion of 
+-- downloading a new random quote.
 function LoadRemoteText( event )
+     -- Have the MessageQueue object create a new message
+     -- using the text in the 'event.response' field.
      myMessageQueue:Add(event.response)
 end
+
 
 function CreateMessage()
      network.request( "http://iheartquotes.com/api/v1/random?format=text&max_lines=4&max_characters=160", "GET", LoadRemoteText )
 end
-timer.performWithDelay(1000, CreateMessage ,-1)
+-- Every 1.5 seconds (1500ms) call the CreateMessage function. The -1 denotes to repeat indefinitely...
+timer.performWithDelay(1500, CreateMessage ,-1)
 
